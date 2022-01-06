c982ea880f0abe366535486e49a13068a0edbb8f
meidlinga <meidlinga@users.noreply.github.com>
2022-01-06 22:28:59 +0100

    remove microg

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -2,12 +2,6 @@
 
 cd "${AOSP_BUILD_DIR}"
 
-echo "applying microg patches"
-cd "${AOSP_BUILD_DIR}/packages/modules/Permission"
-patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00001-fake-package-sig.patch"
-cd "${AOSP_BUILD_DIR}/frameworks/base"
-patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00002-microg-sigspoof.patch"
-
 # apply custom hosts file
 custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
 echo "applying custom hosts file ${custom_hosts_file}"
